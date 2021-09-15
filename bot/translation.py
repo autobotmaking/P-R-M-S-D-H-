e1a59e09d0cdf9ab89e8fef9892d5261202ad29b
autobotmaking <90567635+autobotmaking@users.noreply.github.com>
2021-09-15 10:05:29 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,13 +4,13 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
+    START_TEXT = """<b>Hey {} How Are You 😎!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Just Add Me To Your Group And Channel And Connect Them And See My Pevers 😍
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@Movies_Ready_Back_Up
 </i>"""    
     
     HELP_TEXT = """
@@ -43,12 +43,11 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/mr_no_name_is_here">ADMIN</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
